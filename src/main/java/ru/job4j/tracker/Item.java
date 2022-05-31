6d3544883bb5f666be788d2f12e80d2eb4cd661b
0berwolf <werewolf01051990@gmail.com>
2022-05-31 15:43:47 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,42 +8,20 @@ public class Item {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
+    public void setAll(String name, int id) {
         this.name = name;
         this.id = id;
-    }
-
-    public void getInfo() {
-        System.out.println("enter your name : " + name);
-        System.out.println("enter your id : " + id);
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Item item = new Item();
-        item.getInfo();
-        Item second = new Item("Andrei");
-        second.getInfo();
-        Item third = new Item("Andrei", 2948188);
-        third.getInfo();
     }
 }
