a61ac6a9f43eba6c4055872e83e17c6ac5a8fe57
0berwolf <werewolf01051990@gmail.com>
2022-06-02 21:15:38 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/PizzaShop.java
+++ src/main/java/ru/job4j/oop/PizzaShop.java
@@ -2,11 +2,11 @@ package ru.job4j.oop;
 
 public class PizzaShop {
     public static void main(String[] args) {
-    Pizza pizza = new Pizza();
-    PizzaExtraCheese pizzaExtraCheese = new PizzaExtraCheese();
-    PizzaExtraCheeseExtraTomato pizzaExtraCheeseExtraTomato = new PizzaExtraCheeseExtraTomato();
+        Pizza pizza = new Pizza();
+        PizzaExtraCheese pizzaExtraCheese = new PizzaExtraCheese();
+        PizzaExtraCheeseExtraTomato pizzaExtraCheeseExtraTomato = new PizzaExtraCheeseExtraTomato();
         System.out.println(pizza.name());
         System.out.println(pizzaExtraCheese.name());
         System.out.println(pizzaExtraCheeseExtraTomato.name());
-}
+    }
 }
