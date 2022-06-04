06909cb9ed2878e12ba1d134208637fd2338e768
0berwolf <werewolf01051990@gmail.com>
2022-06-04 14:56:53 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    public LocalDateTime created;
 
     public Item() {
     }
@@ -20,15 +24,35 @@ public class Item {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime created = LocalDateTime.now();
+    }
+
+    public class StartUI {
+        public static void main(String[] args) {
+           Item item = new Item();
+           item.getCreated();
+           DateTimeFormatter getCreated = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        }
     }
 }
