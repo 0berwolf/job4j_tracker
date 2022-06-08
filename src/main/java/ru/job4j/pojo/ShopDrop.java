6e119727c06f5729ef0270d3b339210d8f8f3ddc
0berwolf <werewolf01051990@gmail.com>
2022-06-08 21:51:37 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,15 +3,9 @@ package ru.job4j.pojo;
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
-            Product product = products[i];
             products[i] = products[i + 1];
-            products[i + 1] = null;
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
