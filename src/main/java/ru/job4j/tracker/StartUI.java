27401ad4ecec3d3f7ce49b5fa8e8faf727760289
0berwolf <werewolf01051990@gmail.com>
2022-06-12 21:05:45 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,9 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("unicorn", 25);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter));
+        System.out.println(item);
     }
 }
