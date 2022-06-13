ad8ae333b51cb53c262110548734c3858cfd6052
0berwolf <werewolf01051990@gmail.com>
2022-06-13 09:09:51 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,18 +56,13 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        Item[] items = new Item[size];
         int index = indexOf(id);
         boolean rsl = index != -1;
-        for (int i = index; i < size; i++) {
-            System.arraycopy(items, index + 1, items, index, size - index - 1);
             if (rsl) {
+                System.arraycopy(items, index + 1, items, index, size - index - 1);
                 items[size - 1] = null;
                 size--;
-            } else {
-                return false;
-            }
         }
-        return true;
+        return rsl;
     }
 }
