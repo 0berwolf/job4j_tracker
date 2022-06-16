bb448248e0f57204e307a431cd7e444b9e8f69b6
0berwolf <werewolf01051990@gmail.com>
2022-06-16 15:21:49 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -10,7 +10,7 @@ public class MagicBall {
         String name = input.nextLine();
         System.out.println(name + ", и что ты хотел?");
         System.out.println("загадать число от 0 до 3 ");
-        System.out.print("Ответ при выборе числа: ");
+        System.out.print("Ответ ИИ, при выборе числа: ");
         int answer = new Random().nextInt(3);
         if (answer == 0) {
             System.out.println("Да");
