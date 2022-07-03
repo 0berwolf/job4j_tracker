5f797222ce97ca7746be4bb874a738cf4ea54819
0berwolf <werewolf01051990@gmail.com>
2022-07-03 15:44:16 +0300

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -33,7 +33,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
+        String[] answers = {String.valueOf(item.getId()) };
         StartUI.deleteItem(new StubInput(answers), tracker);
         assertNull(tracker.findById(item.getId()));
     }
