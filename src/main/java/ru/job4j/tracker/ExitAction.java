9353a10b8390098e8fc2e32feedb4dd42a691724
0berwolf <werewolf01051990@gmail.com>
2022-07-04 06:48:21 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/ExitAction.java
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -9,16 +9,8 @@ public class ExitAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        boolean run = true;
-        while (run) {
-            int select = input.askInt("Select: ");
-            if (select != 5) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
-                run = false;
-            }
-        }
-        return run;
+
+        return false;
     }
 }
 
