2fd0f523f409583ffd37681ad363a8cf29335117
0berwolf <werewolf01051990@gmail.com>
2022-07-23 15:26:14 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/FindMaxLength.java
+++ src/main/java/ru/job4j/ex/FindMaxLength.java
@@ -2,7 +2,7 @@ package ru.job4j.ex;
 
 public class FindMaxLength {
     public static void main(String[] args) {
-        String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
+        String[] shops = {"Ebay", null, "Amazon", null, "Ozon" };
         int max = 0;
         for (String el : shops) {
             if ((el != null) && el.length() > max) {
