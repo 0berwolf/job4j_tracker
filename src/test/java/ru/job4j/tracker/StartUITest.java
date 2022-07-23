45fc6abfda3626850faa602bc1a06cc39bef4d9c
0berwolf <werewolf01051990@gmail.com>
2022-07-23 15:22:45 +0300

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -27,7 +27,7 @@ public class StartUITest {
         Item one = tracker.add(new Item("test1"));
         String replaceName = "New Test Name";
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+                new String[] {"0", String.valueOf(one.getId()), replaceName, "1" }
         );
         UserAction[] actions = new UserAction[]{
                 new ReplaceAction(out),
