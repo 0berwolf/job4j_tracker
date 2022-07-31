0204d5d5f6cd30897991ec20354287c0d13616e1
0berwolf <werewolf01051990@gmail.com>
2022-07-31 15:54:19 +0300

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -8,12 +8,12 @@ public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void when3then6() {
-        new Fact().calc(4);
+        new Fact().calc(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenMines1() {
-        int rsl = new Fact().calc(-1);
+        int rsl = new Fact().calc(4);
         assertThat(rsl, is(4));
     }
 }
