eaaff1a4b7d20678121cc4d512b6cd0a984eaf90
0berwolf <werewolf01051990@gmail.com>
2022-08-01 22:04:21 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,17 +4,21 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
-                throw new ElementNotFoundException("not found element");
+            if (key.equals(value[i])) {
+                rsl = i;
             }
+            break;
         }
-        return rsl;
-    }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("not found element");
+        }
+            return rsl;
+        }
 
     public static void main(String[] args) {
         String[] value = {"grand", "cake", "max", "bull"};
         try {
-            indexOf("cake", "text comeback");
+            indexOf(value, "text comeback");
         } catch (ElementNotFoundException k) {
             k.printStackTrace();
         }
