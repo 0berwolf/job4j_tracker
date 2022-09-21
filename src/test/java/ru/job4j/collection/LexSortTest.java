f9e8573e05ea41f133a8fb22467c4ed7b8908635
0berwolf <werewolf01051990@gmail.com>
2022-09-21 20:50:48 +0300

    Сортировка номера

--- src/test/java/ru/job4j/collection/LexSortTest.java
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -13,7 +13,7 @@ class LexSortTest {
         String[] input = {
                 "10. Task.",
                 "2. Task.",
-                "1. Task."};
+                "1. Task." };
         String[] out = {
                 "1. Task.",
                 "2. Task.",
