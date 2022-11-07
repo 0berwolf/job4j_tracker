2ee990ead669362a54568b7c2a098d567606f722
0berwolf <werewolf01051990@gmail.com>
2022-11-07 14:00:46 +0300

    Local-Variable Type Inference

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -2,25 +2,23 @@ package ru.job4j.search;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PhoneDictionaryTest {
 
     @Test
     void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Andrew", "Golub", "336703891", "Gomel"));
-        ArrayList<Person> persons = phones.find("Andrew");
+        var persons = phones.find("Andrew");
         assertThat(persons.get(0).getSurname()).isEqualTo("Golub");
     }
 
     @Test
     void whenNotFound() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Andrew", "Golub", "336703891", "Gomel"));
-        ArrayList<Person> persons = phones.find("Ivan");
+        var persons = phones.find("Ivan");
         assertThat(persons.isEmpty());
     }
 }
